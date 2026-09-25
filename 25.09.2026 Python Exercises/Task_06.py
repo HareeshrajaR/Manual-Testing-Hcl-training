7683014638f9655985b0b85f46f46887de3c1faa
HareeshrajaR <144870459+HareeshrajaR@users.noreply.github.com>
2026-09-25 16:01:17 +0530

    Add function to calculate longest unique substring length
    Implement algorithm to find the length of the longest substring with unique characters.

---
+++ 25.09.2026 Python Exercises/Task_06.py
@@ -0,0 +1,18 @@
+arr = list(map(int, input().split()))
+n = len(arr)
+Set_fun = set()
+left = 0
+maxlen = 0
+for right in range(n):
+    while arr[right] in Set_fun:
+        Set_fun.remove(arr[left])
+        left += 1
+
+    Set_fun.add(arr[right])
+
+    current_len = right - left + 1
+
+    if current_len > maxlen:
+        maxlen = current_len
+
+print(maxlen)
